a36d5c6c94b173173b69838767eff773ab1f79f6
Bruno Gusmão <brunogusmao43@gmail.com>
2022-09-09 14:17:12 -0300

    Array e estruturas de repetição

---
+++ Arrays_estruturas_de_repetição/index.js
@@ -0,0 +1,26 @@
+
+
+const alunos = ['joao','mariana','bruno'];
+
+alunos.push('renan');
+
+alunos[4] = 'vinicius';
+
+console.log(alunos[1]);
+console.log(alunos[2]);
+console.log(alunos[0]);
+
+console.log(alunos);
+
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4] + notas[5];
+
+console.log(soma / 5);
