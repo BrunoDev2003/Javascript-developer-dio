85a31ed1e89c3af6e5034bbab03a76e311caba73
Bruno Gusmão <brunogusmao43@gmail.com>
2022-09-13 15:15:43 -0300

    desafios

---
+++ Arrays_estruturas_de_repetição/desafio2.js
@@ -0,0 +1,9 @@
+// crie um programa que seja capaz de percorrewr uma lista de números e imprima cada múmero encontrado
+
+
+const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    console.log(numero);
+}
