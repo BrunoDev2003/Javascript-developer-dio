6ab05f40a78daa2f9db97f5b47f1549b668a594f
Bruno Gusmão <brunogusmao43@gmail.com>
2022-10-14 15:13:52 -0300

    manipulando o resultado da requesição por uma promise

--- Projeto_Pokedex/assets/js/main.js
+++ Projeto_Pokedex/assets/js/main.js
@@ -3,18 +3,21 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(response) {
-    console.log(response)
-})
-    .catch(function(error) {
-        console.log(error)
+fetch(url)
+    .then((response) => response.json())
+        .then(function (jsonBody) {
+            console.log(jsonBody)
+        })
+    .catch(function (error) {
+        console.error(error)
     })
-    .finally(function() {
-        console.log("Requesição concluida")
+    .finally(function () {
+        console.log('Requesição concluida')
     })
+    
 
     
 
 
-// 04:59
+
 
