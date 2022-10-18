4c2fde8d4609fac318f32dbda72b6ab6a47c8ecf
Bruno Gusmão <brunogusmao43@gmail.com>
2022-10-18 17:39:09 -0300

    separando a API da maniuplação do HTML

--- Projeto_Pokedex/assets/js/main.js
+++ Projeto_Pokedex/assets/js/main.js
@@ -23,27 +23,19 @@ function convertPokemonToLi(pokemon) {
 }
 
 const pokemonList = (document.getElementById('pokemonList'))
-pokemonList.innerHTML += '<li>teste</li>'
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-        .then(function (pokemons) {
-            
-            for (let i = 0; i < pokemons.length; i++) {
-                const pokemon = pokemons[i];
-                pokemonList.innerHTML += convertPokemonToLi(pokemon)
-                
 
-                
-            }
-        })
-        document.getElementsByClassName('pokemons')
-    .catch(function (error) {
-        console.error(error)
-    })
-    .finally(function () {
-        console.log('Requesição concluida')
-    })
+pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
+
+    pokemons.map()
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItems.push(convertPokemonToLi(pokemon))
+    }
+    console.log(listItems)
+})
+        
+        
     
 
     
