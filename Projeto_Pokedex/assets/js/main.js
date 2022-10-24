43251370a4f07dd10b86d50c94b92a81d414d595
Bruno Gusmão <brunogusmao43@gmail.com>
2022-10-24 15:52:11 -0300

    terminado a manipulação de varias requesições

--- Projeto_Pokedex/assets/js/main.js
+++ Projeto_Pokedex/assets/js/main.js
@@ -1,4 +1,10 @@
 
+
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+
 const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
@@ -6,15 +12,14 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-                <span class="number">0001</span>
+                <span class="number">${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${convertPokemonTypesToLi(pokemon.types).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" 
                     alt="${pokemon.name}">
                 </div>
             </li>
