5d68babefddeac95ac6cdebd454094bd9cb07427
Bruno Gusmão <brunogusmao43@gmail.com>
2022-10-25 15:50:08 -0300

    convertendo o modelo da PokeApi para o nosso proprio modelo

--- Projeto_Pokedex/assets/js/main.js
+++ Projeto_Pokedex/assets/js/main.js
@@ -1,9 +1,5 @@
 
 
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 
 const offset = 0
 const limit = 10
@@ -11,13 +7,13 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon) {
     return `
-    <li class="pokemon">
-                <span class="number">${pokemon.order}</span>
+    <li class="pokemon" ${pokemon.type}>
+                <span class="number">${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        ${convertPokemonTypesToLi(pokemon.types).join('')}
+                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                     </ol>
                     <img src="${pokemon.sprites.other.dream_world.front_default}" 
                     alt="${pokemon.name}">
