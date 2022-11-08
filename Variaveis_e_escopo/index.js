7f9718676f0bd2032952ce568924265b0bd465b2
Bruno Gusmão <brunogusmao43@gmail.com>
2022-11-08 15:32:40 -0300

    tipos de dados

--- Variaveis_e_escopo/index.js
+++ Variaveis_e_escopo/index.js
@@ -17,3 +17,26 @@ teste1 = function() {
 
 // o hositing inça as funções para cima das declarações que chamam as funções
 
+//tipos de dados           
+/* 
+    boolean
+    null
+    undefined
+    number
+    string
+    symbol
+*/
+
+//object
+
+let x = 10
+x = 20
+//aqui em cima o valor não é mudado em si, apenas a referencia que muda
+
+let y = {numero: 10}
+y.numero = 20
+
+console.log(10 == '10')
+//aqui em cima ele é uma comparação sem considerar o tipo
+console.log(20 === '20')
+//aqui emc cima é uma comparação considerando o tipo
