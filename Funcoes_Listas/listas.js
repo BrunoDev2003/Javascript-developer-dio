d8921e94b377f6ca2a4c09f58814e535da588873
Bruno Gusmão <brunogusmao43@gmail.com>
2022-11-25 15:12:33 -0300

    Map

--- Funcoes_Listas/listas.js
+++ Funcoes_Listas/listas.js
@@ -10,3 +10,36 @@ for (let i = 0; i < list.length; i++) {
     const element = array[i];
     cb(element,i,list)
 }
+
+const listadeNumerosPares = list.filter(element => {
+    return (element % 2 === 0)
+})
+
+console.log(list)
+console.log(listadeNumerosPares)
+
+class Pessoa {
+    constructor(name) {
+        this.name = name 
+    }
+}
+
+const lista = (new Pessoa('Bruno', new Pessoa('Renan'), new Pessoa('Andreia')))
+
+
+const listaEmHtml = lista.map(element => {
+    return `
+        <li>
+            ${element.name}
+        </li>
+    `
+})
+/*const listaNomes = []
+
+for (let i = 0; 1 < lista.length; i++) {
+    const element = lista[i];
+    listaNomes.push(element.name)
+}
+
+console.log(listaNomes)
+*/
