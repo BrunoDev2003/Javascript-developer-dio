ecada356b69faa3e80410b8f024db552daaba057
Bruno Gusmão <brunogusmao43@gmail.com>
2022-12-26 02:29:13 -0300

    corrigindo mais diretorios

--- docs/assets/js/api.js
+++ docs/assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/BrunoDev2003/Javascript-developer-dio/portfolio-projeto/portfólio/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/BrunoDev2003/Javascript-developer-dio/portfolio-projeto/docs/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
