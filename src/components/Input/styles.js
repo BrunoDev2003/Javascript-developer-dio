4e86e046b8434b764cbbf07b9847abd9b1b89b80
Bruno Gusmão <brunogusmao43@gmail.com>
2022-12-30 18:15:28 -0300

    projeto calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display:flex;
+    align-items:center;
+    justify-conent:flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display:block;
+        direction: ltr;
+        padding: 0 10px;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFF;
+    }
+
+`
