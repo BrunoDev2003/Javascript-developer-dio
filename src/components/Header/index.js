a2862169394746a5494442264c9d607c16a29e54
Bruno Gusmão <brunogusmao43@gmail.com>
2023-01-02 17:12:32 -0300

    initializing the gitfind project with the react lib

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css';
+
+const header = () => {
+  return (
+    <header>
+        <h1>GitFind</h1>
+    </header>
+  )
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import './styles.css';
+import App from './pages/Home';
+import {Header} from '../../components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
