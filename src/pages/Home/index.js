b24d281f5eb8430faef54fc0c6454846494c4334
Bruno Gusmão <brunogusmao43@gmail.com>
2023-01-03 09:56:09 -0300

    implementing the components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,37 @@
 
 import './App.css';
 import './styles.css';
+import background from '../../assets/background.png';
 import App from './pages/Home';
 import {Header} from '../../components/Header';
+import ItemList from '../../components/ItemList';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header>
+        <div className="conteudo">
+          <img src={background} className="background" alt="background-app"></img>
+          <div className="info">
+            <div>
+              <input name="usuario" placeholder="@username"></input>
+              <button>Buscar</button>
+            </div>
+            <div>
+              <img src="https://avatars.githubusercontent.com/u/92750091?v=4" className="profile" alt="imagem de perfil"></img>
+              <div className="perfil">
+                <h3>Bruno Mendonça Gusmão</h3>
+                <p>Descrição placeholder</p>
+              </div>
+            </div>
+            <hr></hr>
+            <div>
+              <h4 className="repositorio">Repósitorios</h4>
+              <ItemList title="Teste" description="teste descrição" />
+            </div>
+          </div>
+        </div>
+      </Header>
     </div>
   );
 }
